afb648d4bd6009dc98a7bb580a9de2991ed80a2f
bangsyir <bangsyir@gmail.com>
2024-01-29 22:28:30 +0800

    move new router

--- restful-api/main.go
+++ restful-api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"golang-restful-api/app"
 	"golang-restful-api/controller"
-	"golang-restful-api/exception"
 	"golang-restful-api/helper"
 	"golang-restful-api/middleware"
 	"golang-restful-api/repository"
@@ -13,25 +12,18 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/go-playground/validator"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 
-	validate := validator.New()
 	db := app.NewDB()
+	validate := validator.New()
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+
 	categoryController := controller.NewCategoryController(categoryService)
-	router := httprouter.New()
+	router := app.NewRouter(categoryController)
 
-	router.GET("/api/categories", categoryController.FindAll)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-
-  router.PanicHandler = exception.ErrorHandler
 	server := http.Server{
 		Addr:    "localhost:3000",
 		Handler: middleware.NewAuthMiddleware(router),
